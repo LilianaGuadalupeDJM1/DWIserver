828c80c785c145405c99c75303ad552594bfd33b
Liliana25072003 <lilianadejesusmartinez3@gmail.com>
2024-07-03 21:06:19 -0600

    controlador

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -32,6 +32,37 @@ export const getUsuarioById = async (req, res) => {
     }
 }
 
+export const changePassword = async (req, res) => {
+    try {
+        const { currentPassword, newPassword } = req.body;
+        const userId = req.params.userId;
+
+        // Obtener el usuario por ID
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'Usuario no encontrado' });
+        }
+
+        // Verificar la contraseña actual
+        const isMatch = await bcrypt.compare(currentPassword, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Contraseña actual incorrecta' });
+        }
+
+        // Encriptar la nueva contraseña
+        const encryptedPassword = await User.encryptPassword(newPassword);
+
+        // Actualizar la contraseña del usuario
+        user.password = encryptedPassword;
+        await user.save();
+
+        res.status(200).json({ message: 'Contraseña actualizada correctamente' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
 
 
 
